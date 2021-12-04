27a5db975f5bcbaae17e09ff83c605ce57b096f3
aminespon <89251553+aminespon@users.noreply.github.com>
2021-12-04 15:37:52 +0000

    Add files via upload

---
+++ React-js-master/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import { Button, Form, FormGroup, Label, Input } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createElement } from "react";
+
+const App = () => {
+  return (
+    <Form>
+      <Form.Group className="Idm" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="Idp" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <center>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </center>
+    </Form>
+  );
+};
+
+export default App;
